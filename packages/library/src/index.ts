f1286e88a1b8879faeaa32378f5b307d26bbe17f
Carlos González <carlos.rgn@gmail.com>
2025-04-30 11:28:49 -0600

    do not export repositories

--- packages/library/src/index.ts
+++ packages/library/src/index.ts
@@ -1,7 +1,5 @@
 export { LibraryModule } from './library.module';
-export { MatchService } from './match/match.service';
-export { MatchRepository } from './match/match.repository';
-export { TeamService } from './team/team.service';
-export { TeamRepository } from './team/team.repository';
-export { ScoreboardService } from './scoreboard/scoreboard.service';
 export { LibraryError } from './library-error';
+export { MatchService } from './match/match.service';
+export { TeamService } from './team/team.service';
+export { ScoreboardService } from './scoreboard/scoreboard.service';
